8bb743fbfadceb7a78a395c3b84ab372b8e14e8a
Ryan <ryan.bout@my.jcu.edu.au>
2023-01-06 14:34:36 +1000

    Add Flask Project

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+@app.route('/f')
+@app.route('/f/<celsius>')
+def f(celsius=""):
+    temp = float(celsius)
+    return f"{convert_to_fahrenheit(temp)}"
+
+
+def convert_to_fahrenheit(celsius):
+    """Convert from Celsius to Fahrenheit and return result"""
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+if __name__ == '__main__':
+    app.run()
